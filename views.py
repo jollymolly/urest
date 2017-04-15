ec520be392b75b9358744b9aff873ed1bc745403
Alexander Saenko <sajenko@gmail.com>
2017-04-15 16:30:47 +0300

    moved files

--- views.py
+++ views.py
@@ -3,9 +3,10 @@ from rest_framework.views import APIView
 from django.db.models import F
 
 from chanalytics.models import (Youtube,
-                         DemographicsAnalytics)
+                                DemographicsAnalytics)
 from .serializers import (YoutubeSerializer,
                           YoutubeDemographicsAnalyticsSerializer)
+
 
 class YoutubeViewSet(viewsets.ModelViewSet):
     serializer_class = YoutubeSerializer
@@ -15,14 +16,15 @@ class YoutubeViewSet(viewsets.ModelViewSet):
         max_rank = self.request.query_params.get('max_rank', None)
 
         queryset = Youtube.objects.annotate(even=F('pk') % 2).filter(
-                       even=0).order_by('-rank')
+            even=0).order_by('-rank')
         if min_rank is not None:
-            queryset = queryset.filter(view_rate__gte=min_rate)
+            queryset = queryset.filter(rank__gte=min_rank)
         if max_rank is not None:
-            queryset = queryset.filter(view_rate__lte=max_rate)
-            
+            queryset = queryset.filter(rank__lte=max_rank)
+
         return queryset
-    
+
+
 class YoutubeDemographicsAnalyticsViewSet(viewsets.ModelViewSet):
     queryset = DemographicsAnalytics.objects.annotate(
         even=F('pk') % 2).filter(even=0).order_by('-viewer_percentage')
